21b7a6dbb293590cbb734316e479d9e4ddfcfe96
Bogdan <114927183+anigilyatornayapushka@users.noreply.github.com>
2026-06-24 00:51:16 +0300

    Update session.py

--- src/db/session.py
+++ src/db/session.py
@@ -2,12 +2,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import decouple
 
 
-DB_NAME = decouple.config('DB_NAME')  # use .env to configure
-DB_PASS = decouple.config('DB_PASS')
-DATABASE_URL = os.getenv('DATABASE_URL', f'postgresql://{DB_NAME}:{DB_PASS}@localhost:5432/backtest')
+DATABASE_URL = db_url = os.getenv("DATABASE_URL", "sqlite:///local_db.sqlite")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
